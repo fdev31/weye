6b31a9f2c3447738f4f8f04b3666f2c6b53c3b01
fab <fdev31@gmail.com>
2013-06-23 21:35:23 +0200

    use plink instead of permalink

--- examples/phonebook/phonebook_app.js
+++ examples/phonebook/phonebook_app.js
@@ -1,6 +1,6 @@
 $.ajax({url: '/d'+ui.doc_ref+'/phone_data.js', dataType: 'json'})
     .done(function(d) {
-        o.html( ich['view_people']({permalink: permalink, child: d})); // render
+        o.html( ich['view_people']({permalink: plink, child: d})); // render
         o.find('.items').isotope({itemSelector: '.item',  layoutMode : 'fitRows'}); // add as isotope list
     })
     .fail(function(e) {
