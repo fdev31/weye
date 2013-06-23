f83000f9f9394ea7493058613e5e187dd5e460f7
fab <fdev31@gmail.com>
2013-06-23 23:57:35 +0200

    UI fixes & improvements
    --HG--
    extra : rebase_source : 8e1314c945a8add0472c99dc0f81fc51b6049ee3

--- examples/phonebook/phonebook_app.js
+++ examples/phonebook/phonebook_app.js
@@ -2,6 +2,7 @@ $.ajax({url: '/d'+ui.doc_ref+'/phone_data.js', dataType: 'json'})
     .done(function(d) {
         o.html( ich['view_people']({permalink: plink, child: d})); // render
         o.find('.items').isotope({itemSelector: '.item',  layoutMode : 'fitRows'}); // add as isotope list
+        $('#addsearch_form').fadeIn();
     })
     .fail(function(e) {
         $.pnotify({text:"Can't load phone data !", type: "error"});
