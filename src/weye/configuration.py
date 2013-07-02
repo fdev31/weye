7d9cca326bce03cb6b31e50890e324f1f1c403fe
fab <fdev31@gmail.com>
2013-07-03 00:20:13 +0200

    configuration read improvements

--- src/weye/configuration.py
+++ src/weye/configuration.py
@@ -29,9 +29,6 @@ config = Config()
 config.static_root = 'static'
 config.shared_root = os.environ.get('ROOT', '/tmp/')
 config.debug = int(os.environ.get('DEBUG', 0)) != 0
-
-config.static_root = config.static_root.rstrip(os.path.sep) # path for static files
-config.shared_db = os.path.join(config.shared_root, '.weye_db') # path for shared files database
 config.special_extension = '.weye'
 config.exclude_dot_files = True
 config.host = '0.0.0.0'
@@ -40,25 +37,34 @@ config.port = '8080'
 # behavior
 config.no_overwrite = os.environ.get('ALLOW_WRITE', '').upper() not in ('1', 'YES', 'TRUE', 'ON')
 
-try:
-    os.mkdir(config.shared_db)
-except (OSError, IOError):
-    pass
 
 _parser = configparser.ConfigParser()
 
 def import_conf(filename=None):
+    root_changed = False
     if filename:
         _parser.read(filename)
         from functools import partial
         rd = partial(_parser.get, 'general')
-        config.shared_root = rd('shared')
-        config.port = int(rd('port'))
-        config.host = rd('host')
+        if rd('shared'):
+            config.shared_root = rd('shared')
+            root_changed = True
+        if rd('port'):
+           config.port = int(rd('port'))
+        if rd('host'):
+            config.host = rd('host')
         config.debug = (rd('debug') or ' ')[0].lower() in 'yta'
         config.no_overwrite = (rd('write') or ' ')[0].lower() not in 'yta'
         if rd('home'):
             os.chdir(rd('home'))
-    config.shared_root = config.shared_root.rstrip(os.path.sep) # path for shared files
+
+    if root_changed or not filename:
+        config.shared_root = config.shared_root.rstrip(os.path.sep) # path for shared files
+        config.shared_db = os.path.join(config.shared_root, '.weye_db') # path for shared files database
+
+        try:
+            os.mkdir(config.shared_db)
+        except (OSError, IOError):
+            pass
 
 import_conf()
