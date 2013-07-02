3e47628997613056e192faaef27e45f0322b8bcf
fab <fdev31@gmail.com>
2013-07-03 00:11:30 +0200

    add simple configuration file support

--- src/weye/__main__.py
+++ src/weye/__main__.py
@@ -1,12 +1,20 @@
 import bottle
 from .application import application
 import weye.configuration
+import sys
 import logging
 log = logging.getLogger('main')
 weye.configuration.config.debug = True
 log.info('starting application in debug mode, run using "uwsgi --ini uwsgi_conf.ini" to avoid those traces.')
 weye.configuration.log.setLevel(logging.NOTSET)
 
+if len(sys.argv) > 1:
+    weye.configuration.import_conf(sys.argv[1])
+
 application.catchall = False
-bottle.run(application, host='0.0.0.0', reloader=False, debug=True)
+bottle.run(application,
+        host=weye.configuration.config.host or '0.0.0.0',
+        reloader=False,
+        debug=weye.configuration.config.debug,
+        port=weye.configuration.config.port or 8080)
 
