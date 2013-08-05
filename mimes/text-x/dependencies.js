638ef166793acd8ecd076e313fda460f6ea13fcd
fab <fdev31@gmail.com>
2013-08-05 20:23:27 +0200

    add support for .patch files

--- mimes/text-x/dependencies.js
+++ mimes/text-x/dependencies.js
@@ -1,9 +1,11 @@
 [
+// Do not forget to EDIT view.js accordingly
 "rainbow.min.js",
 "lang/generic.js",
 "lang/javascript.js",
 "lang/css.js",
 "lang/c.js",
+"lang/diff.js",
 "lang/html.js",
 "lang/lua.js",
 "lang/shell.js"
---
+++ mimes/text-x/js/lang/diff.js
@@ -0,0 +1,24 @@
+/**
+ * Generic language patterns
+ *
+ * @author Craig Campbell
+ * @version 1.0.10
+ */
+Rainbow.extend('diff', [
+        {
+            name: 'comment',
+            pattern: /(diff|\+{3}|-{3}) .*/g
+        },
+        {
+            name: 'comment',
+            pattern: /\\ No newline at end of file/g
+        },
+        {
+            name: 'added',
+            pattern: /\+ .*/g
+        },
+        {
+            name: 'removed',
+            pattern: /- .*/g
+        },
+    ], true);
