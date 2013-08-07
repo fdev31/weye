ec2beaee2355a5f0da0b49d01f0ec99c2c1e196e
fab <fdev31@gmail.com>
2013-08-08 00:43:58 +0200

    Bang ! Bootstrap3-rc1 + improvements + almost no regression ;)

--- mimes/image/view.js
+++ mimes/image/view.js
@@ -1,5 +1,5 @@
 function display(item) {
-    $('<img src="/d'+item.path+'" />').appendTo(
+    $('<img class="img-responsive" src="/d'+item.path+'" />').appendTo(
         $('#contents').html( get_view('file', item) )
     );
     ui.set_context('item');
