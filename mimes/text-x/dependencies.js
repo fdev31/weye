8b69bdb199ce7742e1c0b152d120deb87b21408a
fab <fdev31@gmail.com>
2013-08-27 01:04:18 +0200

    somehow refactor highlighted text
    --HG--
    branch : wip-refactor
    rename : mimes/text-x/js/lang/c.js => mimes/text-x/orig/lang/c.js
    rename : mimes/text-x/js/lang/css.js => mimes/text-x/orig/lang/css.js
    rename : mimes/text-x/js/lang/diff.js => mimes/text-x/orig/lang/diff.js
    rename : mimes/text-x/js/lang/generic.js => mimes/text-x/orig/lang/generic.js
    rename : mimes/text-x/js/lang/html.js => mimes/text-x/orig/lang/html.js
    rename : mimes/text-x/js/lang/javascript.js => mimes/text-x/orig/lang/javascript.js
    rename : mimes/text-x/js/lang/lua.js => mimes/text-x/orig/lang/lua.js
    rename : mimes/text-x/js/lang/shell.js => mimes/text-x/orig/lang/shell.js

--- mimes/text-x/dependencies.js
+++ mimes/text-x/dependencies.js
@@ -1,13 +1,5 @@
 [
 // Do not forget to EDIT view.js accordingly
 "myobj.js",
-"rainbow.min.js",
-"lang/generic.js",
-"lang/javascript.js",
-"lang/css.js",
-"lang/c.js",
-"lang/diff.js",
-"lang/html.js",
-"lang/lua.js",
-"lang/shell.js"
+"rainbow.js"
 ]
