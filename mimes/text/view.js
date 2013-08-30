3946b2a341f4dc0cf135f5cbf1c85770cec32d9b
fab <fdev31@gmail.com>
2013-08-30 22:25:51 +0200

    add some fancyness to the save button
    --HG--
    branch : wip-refactor

--- mimes/text/view.js
+++ mimes/text/view.js
@@ -4,7 +4,7 @@ function display(item) {
     $('<div class="row-fluid"><small>Fullscreen: <i>Alt+F</i>, Toggle preview: <i>Alt+P</i></small></div><div class="row-fluid" id="epiceditor"></div> <div class="pull-right btn-group"></div>').appendTo($('#contents'));
 
     $('<button class="btn btn-success btn-large" onclick="editor_save()">Save changes</button>')
-        .appendTo( $('#download_link').parent() );
+        .appendTo( $('#download_link').parent() ).hide().fadeIn();
 
     var ajax_call = new $.ajax({url: '/d'+item.get_ref(), dataType: 'text'})
     .fail(function(e) {
